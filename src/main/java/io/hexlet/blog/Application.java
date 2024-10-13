297c723dc790332692fb4508a6f51358dd1c7680
Arkadii <arkadii.malygin@gmail.com>
2024-10-13 21:54:35 +0300

    initial

--- src/main/java/io/hexlet/blog/Application.java
+++ src/main/java/io/hexlet/blog/Application.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import net.datafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping("/")
+    String home() {
+        return "Hello World";
+    }
     @Bean
     public Faker getFaker() {
         return new Faker();
